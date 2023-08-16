577119fddb49994ab399144dcc7ca8cdc16dfb29
AquazX20 <alejandroarga2001@gmail.com>
2023-08-16 13:17:53 -0600

    RFID added

--- RFID/RFID.ino
+++ RFID/RFID.ino
@@ -1,9 +1,35 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 5
+#define RST 27
+
+MFRC522 rfid(SS_PIN, RST);
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  SPI.begin();
+  rfid.PCD_Init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
 
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+  }
 }
